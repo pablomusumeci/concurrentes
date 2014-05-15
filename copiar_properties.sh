07b3b1a3037e68d29ffa1ccf65de8fca139f3987
Javier Choque <jchoque@fi.uba.ar>
2014-05-15 00:49:14 -0300

    script para copia de properties

---
+++ copiar_properties.sh
@@ -0,0 +1,5 @@
+cp application.properties procesoPrincipal/
+cp application.properties procesoGeneradorAutos/
+cp application.properties procesoAdministrador/
+cp application.properties procesoEmpleado/
+cp application.properties procesoJefeEstacion/
